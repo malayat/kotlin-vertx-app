fa15fff5d1c4dbc4505a439c770fbe44cf015e2a
Alejandro Ayala Ati <maaa220817@sri.ad>
2018-06-06 12:09:22 -0500

    Change to kotlin jdk 8, add thymeleaf template engine and now de index page show the current time using java.time(java 8)

--- src/main/kotlin/HelloVertx.kt
+++ src/main/kotlin/HelloVertx.kt
@@ -1,15 +1,27 @@
 import io.vertx.core.Vertx
 import io.vertx.ext.web.Router
+import io.vertx.ext.web.templ.ThymeleafTemplateEngine
 import org.slf4j.LoggerFactory
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.time.format.FormatStyle
 
 fun main(args: Array<String>) {
     val vertx = Vertx.vertx()
     val server = vertx.createHttpServer()
     val router = Router.router(vertx)
     val logger = LoggerFactory.getLogger("VertxServer")
+    val templateEngine = ThymeleafTemplateEngine.create()
 
-    router.get("/").handler {
-        it.response().end("Hello Vertx and Router with Kotlin")
+    router.get("/").handler { ctx ->
+        ctx.put("time", LocalDateTime.now().format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM)))
+        templateEngine.render(ctx, "public/templates/", "index.html") { buf ->
+            if (buf.failed()) {
+                logger.error("Template rendering failed.", buf.cause())
+            } else {
+                ctx.response().end(buf.result())
+            }
+        }
     }
 
     server.requestHandler {
