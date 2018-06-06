1ffe8acc3bb4ff936b767cf42cc1213d165eb8f3
Alejandro Ayala Ati <maaa220817@sri.ad>
2018-06-06 11:01:57 -0500

    Add Vert.x router

---
+++ README.md
@@ -0,0 +1,17 @@
+# Vetx app with kotlin
+
+Simple example of [Vert.x](https://vertx.io/) with [Kotlin](https://kotlinlang.org/).
+
+## Run
+
+1. ``gradle build``
+2. ``gradle run``
+3. Open [localhost:9090](http://localhost:9090/)
+
+## To do
+
+- Working with Vert.x
+- Frontend integration
+- Authentication users
+- Testing
+- Hosting and deploy
--- src/main/kotlin/HelloVertx.kt
+++ src/main/kotlin/HelloVertx.kt
@@ -1,10 +1,22 @@
 import io.vertx.core.Vertx
+import io.vertx.ext.web.Router
+import org.slf4j.LoggerFactory
 
 fun main(args: Array<String>) {
-    val server = Vertx.vertx().createHttpServer()
+    val vertx = Vertx.vertx()
+    val server = vertx.createHttpServer()
+    val router = Router.router(vertx)
+    val logger = LoggerFactory.getLogger("VertxServer")
+
+    router.get("/").handler {
+        it.response().end("Hello Vertx and Router with Kotlin")
+    }
+
     server.requestHandler {
-        it.response().end("Hello Vertx with Kotlin!")
+        router.accept(it)
     }.listen(9090) {
-        if (!it.succeeded()) println("Failed to listen on port 9090")
+        if (!it.succeeded()) {
+            logger.error("Failed to listen on port 9090. {}", it.cause().message)
+        }
     }
 }
